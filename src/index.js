c59a377d0d2341acc65731421d03ece8ff3b77bd
MennaBashir <mennabashir00@gmail.com>
2024-03-17 20:02:52 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { store } from "./Redux/store";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
